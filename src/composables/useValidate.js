7a411afbe4970161fc6d13bd4bc9a989419f8a9b
Askar Assetov <comgraph.aa@gmail.com>
2021-09-30 08:43:35 +0600

    customization of validations

--- src/composables/useValidate.js
+++ src/composables/useValidate.js
@@ -1,15 +1,24 @@
 import { reactive } from "@vue/reactivity";
 import useVuelidate from "@vuelidate/core";
-import { required, email, minLength } from "@vuelidate/validators";
+import { required, email, minLength, helpers } from "@vuelidate/validators";
 
 export default function useValidate() {
   const state = reactive({
     email: "",
     password: "",
   });
+  const customRequired = helpers.withMessage("Обязательное поле", required);
+  const customEmail = helpers.withMessage(
+    "Значение не является допустимым адресом электронной почты",
+    email
+  );
+  const customMinLength = helpers.withMessage(
+    "Это поле должно быть не менее 6 символов",
+    minLength(6)
+  );
   const rules = {
-    email: { required, email },
-    password: { required, minLength: minLength(6) },
+    email: { customRequired, customEmail },
+    password: { customRequired, customMinLength },
   };
   const v$ = useVuelidate(rules, state);
 
